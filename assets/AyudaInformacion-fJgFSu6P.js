d9e15a99773ce60f898f08d592ac7acc1bfdd49d
yohan ryuking <yohanryuking@gmail.com>
2024-02-03 23:48:30 -0500

    Updates

--- assets/AyudaInformacion-fJgFSu6P.js
+++
@@ -1,7 +0,0 @@
-import{j as e,r as t,_ as r}from"./vendor-TeGnUX-f.js";import{f as n,h as o,T as s}from"./index-IetW687g.js";const a=t.lazy(()=>r(()=>import("./AyudaInformacionContainer-cZ7tkLHK.js"),__vite__mapDeps([0,1])));function u(){return e.jsx("div",{style:n,className:"container flex z-40 w-full h-auto items-center justify-center data-[menu-open=true]:border-none  top-0 inset-x-0   backdrop-blur-lg data-[menu-open=true]:backdrop-blur-xl backdrop-saturate-150 bg-background/70",children:e.jsx("section",{className:"section px-6",style:o,children:e.jsxs(t.Suspense,{children:[e.jsx(s,{title:"Preguntas más comunes"}),e.jsx(a,{})]})})})}export{u as default};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["assets/AyudaInformacionContainer-cZ7tkLHK.js","assets/vendor-TeGnUX-f.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
